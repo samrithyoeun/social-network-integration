7f6e6a8a45fe59f384f99304829bdfab768c28a4
samrith <samrith.yoeun@gmail.com>
2018-07-20 08:06:23 +0700

    finish twitter

---
+++ twitter login/AppDelegate.swift
@@ -0,0 +1,67 @@
+//
+//  AppDelegate.swift
+//  twitter login
+//
+//  Created by Samrith Yoeun on 7/17/18.
+//  Copyright © 2018 samrith. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+import FBSDKLoginKit
+import GoogleSignIn
+import OAuthSwift
+import LinkedinSwift
+import CloudrailSI
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    static let kCloudRailAPIKey = "5b500df73ef31211abdc22d3"
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        CRCloudRail.setAppKey(AppDelegate.kCloudRailAPIKey)
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        GIDSignIn.sharedInstance().clientID = Config.googleClientId
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        if (url.host == "oauth-callback") {
+            OAuthSwift.handle(url: url)
+        }
+        
+        if LinkedinSwiftHelper.shouldHandle(url) {
+            return LinkedinSwiftHelper.application(app, open: url, sourceApplication: nil, annotation: nil)
+        }
+        
+        _ = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+        
+        return true
+    }
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ twitter login/Model/Authentication /AuthenticationManager.swift
@@ -0,0 +1,210 @@
+//
+//  AuthenticationManager.swift
+//  login social
+//
+//  Created by PM Academy 3 on 7/16/18.
+//  Copyright © 2018 PM Academy 3. All rights reserved.
+//
+
+import Foundation
+import GoogleSignIn
+import SwiftyJSON
+import FBSDKLoginKit
+import TwitterKit
+import Alamofire
+import OAuthSwift
+import LinkedinSwift
+import CloudrailSI
+
+class AuthenticationManager: NSObject {
+    static let shared = AuthenticationManager()
+    let fbLoginManager = FBSDKLoginManager()
+    var googleDelegate: GoogleSignInDelegate?
+    var oauthswift: OAuth1Swift?
+    var linkedinHelper: LinkedinSwiftHelper?
+    var instagram = Instagram.init(clientId: Config.instagramKey, clientSecret: Config.instagramSecret)
+    var twitter = Twitter.init(clientId: Config.twitterConsumerKey, clientSecret: Config.twitterConsumerSecret)
+    var github = GitHub.init(clientId: Config.githubKey, clientSecret: Config.githubSecret)
+    var linkedin = LinkedIn.init(clientId: Config.linkedKey, clientSecret: Config.linkedSecret)
+    
+    typealias ResponseUser = ( (_ user: User) -> () )
+    
+    override init() {
+        super.init()
+        
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        
+        let linkedInSwitHelperConfiguration = LinkedinSwiftConfiguration(
+            clientId: Config.linkedKey,
+            clientSecret: Config.linkedSecret,
+            state: "DLKDJF45DIWOERCM",
+            permissions: ["r_basicprofile", "r_emailaddress"],
+            redirectUrl: "https://github.com/tonyli508/LinkedinSwift")
+        
+        linkedinHelper = LinkedinSwiftHelper(configuration: linkedInSwitHelperConfiguration!)
+    }
+    
+    func facebookLogin (from uiViewController: UIViewController, completion: @escaping (Result<User>) -> () ) {
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: uiViewController) { (result, error) -> Void in
+            if (error == nil) {
+                if (result?.isCancelled)! {
+                    print("athentication: user cancelled Facebook log in ")
+                    return
+                } else {
+                    FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "email, name, picture.type(large)"])
+                        .start(completionHandler: { (connection, result, error) -> Void in
+                            if (error == nil){
+                                var user = self.mapData(result)
+                                print("facebook authentication")
+                                print(user)
+                                user.token = FBSDKAccessToken.current().tokenString
+                                completion(Result.success(user))
+                            } else {
+                                completion(Result.failure("authenticaton: cannot get the user data from Facebook"))
+                            }
+                        })
+                }
+            }
+        }
+    }
+    
+    func mapData(_ value: Any?) -> User {
+        let json = JSON(value ?? "")
+        let id = json["id"].stringValue
+        let name = json["name"].stringValue
+        let email = json["email"].stringValue
+        let user = User(name: name, id: id, token: "", email: email)
+        return user
+    }
+    
+    func googleSignIn() {
+        GIDSignIn.sharedInstance().signIn()
+        
+    }
+    
+    func googleSignOut() {
+        GIDSignIn.sharedInstance().signOut()
+    }
+    
+    func twitterUniversalLogin(callback: ResponseUser){
+        oauthswift = OAuth1Swift(
+            consumerKey: Config.twitterConsumerKey,
+            consumerSecret: Config.twitterConsumerSecret,
+            requestTokenUrl: "https://api.twitter.com/oauth/request_token",
+            authorizeUrl: "https://api.twitter.com/oauth/authorize",
+            accessTokenUrl: "https://api.twitter.com/oauth/access_token")
+        let state = generateState(withLength: 20)
+        
+        oauthswift?.authorize(
+            withCallbackURL: "http://oauthswift.herokuapp.com/callback/twitter",
+            success: { credential, response, parameters in
+                print("logged in with \(credential), with response \(response) and parameters \(parameters)")
+                self.testTwitter(self.oauthswift!)
+        },
+            failure: { error in
+                print("error occured \(error.localizedDescription)")
+        }
+        )
+    }
+    
+    func testTwitter(_ oauthswift: OAuth1Swift) {
+        let _ = oauthswift.client.get(
+            "https://api.twitter.com/1.1/statuses/mentions_timeline.json", parameters: [:],
+            success: { response in
+                let jsonDict = try? response.jsonObject()
+                print(String(describing: jsonDict))
+                
+        }, failure: { error in
+            print(error)
+        }
+        )
+    }
+    
+    func gitHubLogIn(callback: ResponseUser) {
+        do {
+            try github.login()
+            let name  = try github.fullName()
+            let id = try github.identifier()
+            let email = try github.email()
+            let user = User(name: name, id: id, token: "", email: email)
+            print("github user")
+            print(user)
+            callback(user)
+        } catch let error {
+            print("github error")
+            print(error)
+        }
+    }
+    
+    func linkedInLogin(callback: ResponseUser) {
+        do {
+            try linkedin.login()
+            let name  = try linkedin.fullName()
+            let email = try linkedin.email()
+            let id = try linkedin.identifier()
+            let credential = try linkedin.saveAsString()
+            let user = User(name: name, id: id, token: credential, email: email)
+            print(user)
+            callback(user)
+        } catch let error {
+            print("linked error")
+            print(error)
+        }
+    }
+    
+    func instagramLogin(callback: ResponseUser){
+        do {
+            try instagram.login()
+            let name  = try instagram.fullName()
+            let id = try instagram.identifier()
+            
+            let user = User(name: name, id: id, token: "", email: "")
+            print(user)
+            callback(user)
+        } catch let error {
+            print("instagram error")
+            print(error)
+        }
+    }
+    
+}
+
+extension AuthenticationManager: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            print("\(error.localizedDescription)")
+            NotificationCenter.default.post(
+                name: Notification.Name(rawValue: "ToggleAuthUINotification"), object: nil, userInfo: nil)
+        } else {
+            let userId = user.userID
+            let token = user.authentication.idToken
+            let fullName = user.profile.name
+            let email = user.profile.email
+            _ = user.profile.imageURL(withDimension: 200)
+            let user = User(name: fullName!, id: userId!, token: token!, email: email!)
+            googleDelegate?.googleSignInResponse(user: user)
+        }
+        
+    }
+    
+}
+
+extension AuthenticationManager: GIDSignInUIDelegate {
+    func sign(_ signIn: GIDSignIn!, present viewController: UIViewController!) {
+        googleDelegate?.googleSignInLaunch(viewController)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, dismiss viewController: UIViewController!) {
+        googleDelegate?.googleSignInDismiss(viewController)
+    }
+    
+    
+}
+
+
+
+
+
+
